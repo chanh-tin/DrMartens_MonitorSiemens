1782972b2eeaf3bb046087812d326b4bdf262af3
TinVo-isoft <tinvo@i-soft.com.vn>
2024-11-08 14:19:34 +0700

    RemoveSrcV2

--- src/Ver2/deploy/deploy-dev.sh
+++
@@ -1,5 +0,0 @@
-#!bin/bash
-
-cd deploy/
-docker compose down
-docker compose up -d
--- src/Ver2/src/DRMT.MMS.Main/Models/RequestModels/ParameterRequestPagingModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using iSoft.Common.Models.RequestModels;
-
-namespace SourceBaseBE.MainService.Models.RequestModels
-{
-	public class ParameterRequestPagingModel : PagingRequestModel
-	{
-		public int? DeviceId { get; set; }
-	}
-}
