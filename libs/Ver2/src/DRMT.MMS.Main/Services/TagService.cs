2e0034ed253a284c425f8708dab5950013d425ca
TinVo-isoft <tinvo@i-soft.com.vn>
2024-11-19 16:29:14 +0700

    Change Format

--- libs/Ver2/src/DRMT.MMS.Main/Services/TagService.cs
+++ libs/Ver2/src/DRMT.MMS.Main/Services/TagService.cs
@@ -1,68 +1,40 @@
-using iSoft.DBLibrary.DBConnections.Factory;
-using iSoft.Common.Enums.DBProvider;
-using Serilog;
-using iSoft.Common.ConfigsNS;
-using SourceBaseBE.Database.Repository;
-using SourceBaseBE.Database.DBContexts;
-using System;
-using iSoft.Common.Exceptions;
-using System.Data;
-using Microsoft.EntityFrameworkCore.Storage;
-using iSoft.Common.Models.RequestModels;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations.Schema;
-using iSoft.Database.Entities;
-using System.Linq;
-using iSoft.Database.Extensions;
-using iSoft.Common.Models;
-using SourceBaseBE.Database.Entities;
-using static iSoft.Common.ConstCommon;
-using iSoft.Common;
-using iSoft.Common.Enums;
-using iSoft.Database.Models;
-using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
-using iSoft.Common.Utils;
-using iSoft.Database.DBContexts;
-using iSoft.RabbitMQ.Services;
-using static iSoft.Common.Messages;
-using iSoft.Common.Payloads;
-using iSoft.AspNetCore.RabbitMQ;
-using iSoft.AspNetCore.Enums;
+using SourceBaseBE.Database.DBContexts;
+using SourceBaseBE.Database.Repository;
 
 namespace SourceBaseBE.MainService.Services
 {
-    public class TagService : BaseTagService
+  public class TagService : BaseTagService
+  {
+    protected TagRepository _repositoryImp;
+    public TagService(
+        CommonDBContext dbContext,
+        ILogger<TagService> logger
+        )
+        : base(dbContext, logger)
     {
-        protected TagRepository _repositoryImp;
-        public TagService(
-            CommonDBContext dbContext,
-            ILogger<TagService> logger
-            )
-            : base(dbContext, logger)
-        {
-            _repositoryCRUD = new TagRepository(_dbContextImp);
-            _repositoryBase = (TagRepository)(_repositoryCRUD);
-            _repositoryImp = (TagRepository)_repositoryBase;
-            this._dataBlockRepository = new DataBlockRepository(dbContext);
-            
-        }
+      _repositoryCRUD = new TagRepository(_dbContextImp);
+      _repositoryBase = (TagRepository)(_repositoryCRUD);
+      _repositoryImp = (TagRepository)_repositoryBase;
+      this._dataBlockRepository = new DataBlockRepository(dbContext);
 
-        public override string GetServiceName()
-        {
-            return nameof(TagService);
-        }
-
-        //* SyncServiceTransit flag
-        public override bool IsSyncServiceTransit
-        {
-            get { return false; }
-        }
-
-        //* multiLanguage flag
-        public override bool IsMultiLanguage
-        {
-            get { return false; }
-        }
     }
+
+    public override string GetServiceName()
+    {
+      return nameof(TagService);
+    }
+
+    //* SyncServiceTransit flag
+    public override bool IsSyncServiceTransit
+    {
+      get { return false; }
+    }
+
+    //* multiLanguage flag
+    public override bool IsMultiLanguage
+    {
+      get { return false; }
+    }
+  }
 }
